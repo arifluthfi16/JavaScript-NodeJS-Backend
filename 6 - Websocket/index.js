a0c4eb944ca319099604321e10afa380656ca918
Luthfi Salman Arif S <arifluthfi16@gmail.com>
2020-04-14 23:20:26 +0700

    Finished Express Setup

--- 6 - Websocket/index.js
+++ 6 - Websocket/index.js
@@ -1,1 +1,16 @@
-// Intialize web socket
+const express = require('express');
+
+// App Setup
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+const server = app.listen(PORT, ()=>{
+  console.log(`Server Listening on PORT ${PORT}`);
+});
+
+// Static Files
+app.use(express.static('public'))
+
+app.get('/', function(req,res){
+  res.render('index.html')
+})
