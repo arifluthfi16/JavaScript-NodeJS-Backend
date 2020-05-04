b78462de5792259db1f94a038d0e84a67d16297b
Luthfi Salman Arif S <arifluthfi16@gmail.com>
2020-05-04 22:30:28 +0700

    Finished add and remove with using AVL and Queue(FIFO)

--- 8 - NodeJS Orderbook/struct.js
+++ 8 - NodeJS Orderbook/struct.js
@@ -1,3 +1,20 @@
-// Creating order book with AVL 
-// This is using library
+import AVLTree from 'avl';
 
+// var t = new AVLTree(); 
+
+// Order Book Structure
+
+function createNewOrder(price, amount){
+    // Constrcut new orderbook
+    let limitOrder = {
+        price,
+        amount,
+        orderTime : Date.now()
+    }
+
+    t.insert(price, limitOrder);
+}
+
+for(i=1; i<10; i++){
+    createNewOrder(Math.floor(Math.random() * 100),  i);
+}
